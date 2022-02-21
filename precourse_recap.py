e21ffd0206dcc4df1712360d545e1ed23a36c222
Kevin <kevinmuldoon@gmail.com>
2022-02-21 21:46:17 +0000

    My first homework commit.

---
+++ precourse_recap.py
@@ -0,0 +1,31 @@
+# Kevin Muldoon - E56 Homework - Week 1, Day 1
+
+my_number = 7
+my_float = 5.2
+my_string = "Hello CodeClan!"
+
+# Display sum of numbers
+
+my_number_total = my_number + my_float
+print ("The sum of", my_number, "and" ,my_float, "is" ,my_number_total, ":)")
+
+# Pointless number fact!
+
+if my_number_total > 100:
+    print ("That's a large number. Yay!")
+else:
+    print (my_number_total, "is a low number. Boo!")
+
+# Display hello message
+
+if not my_string:
+    print ("No nice hello message for you!")
+else:
+    print (my_string)
+    
+ # Display character count fact
+
+
+print ("Did you know my nice welcome message above has", len(my_string), "characters.")
+
+# End of homework :)
